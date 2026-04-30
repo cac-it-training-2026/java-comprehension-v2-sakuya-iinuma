fbb4cbd7085b51c03b223f004f0fce5d96ba245c
iinumas <iinumas@C010422926.zipangu.cac.co.jp>
2026-04-30 12:31:43 +0900

    0430 q01_basic.question6 まで完了
    理解できた点
    ・実行の流れ
    ・try-catch-finally
    
    不明な点、調査中
    ・固有の例外クラスの内容の把握

---
+++ java_comprehension_exercises_volume2_NO_NAME/src/q01_basic/question06/Member.java
@@ -0,0 +1,96 @@
+package q01_basic.question06;
+
+public class Member extends AbstMember {
+	private int id;
+
+	/**
+	 * @return id
+	 */
+	public int getId() {
+		return id;
+	}
+
+	/**
+	 * @param id セットする id
+	 */
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	/**
+	 * @return password
+	 */
+	public String getPassword() {
+		return password;
+	}
+
+	/**
+	 * @param password セットする password
+	 */
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	/**
+	 * @return age
+	 */
+	public int getAge() {
+		return age;
+	}
+
+	/**
+	 * @param age セットする age
+	 */
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	/**
+	 * @return rank
+	 */
+	public int getRank() {
+		return rank;
+	}
+
+	/**
+	 * @param rank セットする rank
+	 */
+	public void setRank(int rank) {
+		this.rank = rank;
+	}
+
+	private String password;
+	private int age;
+	private int rank;
+
+	public Member() {
+
+	}
+
+	public Member(int id, String password, String name, int age, int rank) {
+		this.id = id;
+		this.password = password;
+		this.name = name;
+		this.age = age;
+		this.rank = rank;
+
+	}
+
+	@Override
+	public void buyItem() {
+
+	}
+
+	@Override
+	public void showMember() {
+		System.out.println("***MEMBER DATA***");
+		System.out.println("id:" + this.id);
+		System.out.println("password:" + this.password);
+		System.out.println("name:" + this.name);
+		System.out.println("age:" + this.age);
+		System.out.println("rank:" + this.rank);
+		System.out.println("*****************");
+
+	}
+
+}
